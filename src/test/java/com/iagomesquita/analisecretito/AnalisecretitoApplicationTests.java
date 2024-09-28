9e74077bc07d2d22b498d95eb6f478de2e28a258
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-27 22:57:26 -0300

    first commit

---
+++ src/test/java/com/iagomesquita/analisecretito/AnalisecretitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagomesquita.analisecretito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisecretitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
