23d27968b253620e69946ff3365ddd309baa93e3
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-30 09:27:06 -0300

    feat: add order in ID to Spring with @Order

--- src/main/java/com/iagomesquita/analisecredito/service/strategy/impl/ScoreImpl.java
+++ src/main/java/com/iagomesquita/analisecredito/service/strategy/impl/ScoreImpl.java
@@ -3,8 +3,10 @@ package com.iagomesquita.analisecredito.service.strategy.impl;
 import com.iagomesquita.analisecredito.domain.Proposal;
 import com.iagomesquita.analisecredito.service.strategy.PointsCalculation;
 import java.util.Random;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(2)
 @Component
 public class ScoreImpl implements PointsCalculation {
 
