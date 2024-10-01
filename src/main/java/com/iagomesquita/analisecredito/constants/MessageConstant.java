21c6736c4fd169a304d3a5cc79631798e2d5b03c
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-10-01 07:35:03 -0300

    feat: add messages constants

---
+++ src/main/java/com/iagomesquita/analisecredito/constants/MessageConstant.java
@@ -0,0 +1,8 @@
+package com.iagomesquita.analisecredito.constants;
+
+public class MessageConstant {
+
+  public static final String NEGATIVE_CUSTUMER = "Operação não permitida. Cliente %s com nome negativo.";
+  public static final String LOW_SERASA_SCORE = "Operação não permitida. Cliente %s com score baixo de 200 pontos.";
+
+}
